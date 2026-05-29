869223f70aeda5214fe11d0166c920fca994b95b
keaneozzie <ozziedharma897@gmail.com>
2026-05-29 16:08:20 +0700

    bikin fungsi dasar buat insert data

--- queue.cpp
+++ queue.cpp
@@ -16,4 +16,12 @@ public:
         REAR = -1;
     }
 
-    
+    void insert()
+    {
+        int num;
+
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        
