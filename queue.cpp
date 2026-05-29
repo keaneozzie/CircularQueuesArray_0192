1d5dd504110971a305db28e451ff7636700bebfa
keaneozzie <ozziedharma897@gmail.com>
2026-05-29 16:14:22 +0700

    membetulkan logika front biar reset ke -1

--- queue.cpp
+++ queue.cpp
@@ -61,4 +61,12 @@ public:
         cout << "\nThe element deleted from the queue is: "
              << queue_array[FRONT] << "\n";
 
-        
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            
