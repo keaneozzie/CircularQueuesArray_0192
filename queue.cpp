e51d598f028e5a67e1af7c5d5505e607ce99b29c
keaneozzie <ozziedharma897@gmail.com>
2026-05-29 16:13:02 +0700

    menambahkan pengecekan queue underflow

--- queue.cpp
+++ queue.cpp
@@ -51,4 +51,14 @@ public:
 
     void remove()
     {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow";
+            return;
+        }
+
+        cout << "\nThe element deleted from the queue is: "
+             << queue_array[FRONT] << "\n";
+
         
